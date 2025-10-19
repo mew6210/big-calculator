d8c5c2ce3cd667804e1bd1949c54f037b0ed52bf
mew6210 <mew.62@wp.pl>
2025-10-19 17:45:01 +0200

    feat(bigint) - other constructors
    - added constructors for:
       - empty params
       -uint64

--- src/bigint/bigint.cpp
+++ src/bigint/bigint.cpp
@@ -1,14 +1,34 @@
 #include "bigint.hpp"
 
+void checkBigInt() {
+
+	BigInt a = BigInt();
+	BigInt b = BigInt("");
+	BigInt c = BigInt(25);
+	c.inspectChunks();
+}
 
 BigInt::BigInt(const std::string& s) {
 	chunks = {};
 	isPositive = true;
 }
 
+BigInt::BigInt(const uint64_t& u64Val){
+	chunks.resize(2);
+	chunks[0] = u64Val;
+	isPositive = true;
+}
+
+BigInt::BigInt() {
+	chunks = {};
+	isPositive = true;
+}
+
 void BigInt::inspectChunks(){
 	if(chunks.size()==0) return;
+	std::cout << "------------\n";
 	for(int i=0;i<chunks.size();i++){
 		std::cout<<"chunk "<<i<<": "<<chunks[i]<<"\n";
 	}
+	std::cout << "------------\n";
 }
--- src/bigint/bigint.hpp
+++ src/bigint/bigint.hpp
@@ -3,12 +3,16 @@
 #include <string>
 #include <iostream>
 
+void checkBigInt();
+
 class BigInt {
 
 	std::vector<uint64_t> chunks;
 	bool isPositive;
 
 public:
-	BigInt(const std::string&);
+	BigInt(const std::string& s);
+	BigInt(const uint64_t& u64Val);
+	BigInt();
 	void inspectChunks();
 };
