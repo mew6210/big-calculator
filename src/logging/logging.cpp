57a70f4ca8d19e7d592f10b767a3c404db5f7014
mew6210 <mew.62@wp.pl>
2025-11-20 20:36:37 +0100

    doc(logging)

--- src/logging/logging.cpp
+++ src/logging/logging.cpp
@@ -1,4 +1,5 @@
 #include "logging.hpp"
+#include <iostream>
 
 void printErrType(const ErrType& type) {
     switch (type) {
@@ -10,22 +11,20 @@ void printErrType(const ErrType& type) {
 }
 
 void printError(const ErrMsg& errMsg) {
-    std::cout << errMsg.ctx << "\n";
-    /*
-    arrow to point where error was
-    */
-    for (int j = 0; j < 3; j++) {
+    std::cout << errMsg.ctx << "\n";    //print entire line
+    
+    for (int j = 0; j < 3; j++) {   // arrow to point where error was
         for (int i = 0; i <= errMsg.ctx.size(); i++) {
-            if (j == 0 && i == errMsg.pointerPos) {
+            if (j == 0 && i == errMsg.pointerPos) {     //if its first row, then print arrow
                 std::cout << "^";
                 continue;
             }
-            if (i == errMsg.pointerPos) {
+            if (i == errMsg.pointerPos) {               //if column is where position of error is, print arrow base
                 std::cout << "|";
             }
             else std::cout << " ";
         }
-        std::cout << "\n";
+        std::cout << "\n";  //next row
     }
 
     std::cout << "\n";
--- src/logging/logging.hpp
+++ src/logging/logging.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include <string>
-#include <iostream>
 
 enum class ErrType {
 	Lexer,
@@ -9,11 +8,12 @@ enum class ErrType {
 };
 
 struct ErrMsg {
-	std::string ctx;
-	uint64_t pointerPos;
-	std::string msg;
-	std::string note;
-	ErrType type;
+	std::string ctx;		//entire line of user input
+	uint64_t pointerPos;	//position where to display an arrow
+	std::string msg;		//error message
+	std::string note;		//hopefully helpful note how to fix it
+	ErrType type;			//from what part of an app the error is coming from
+	
 	ErrMsg(const std::string& c,const uint64_t p,const std::string& m,const std::string& n,const ErrType e): 
 		ctx(c),pointerPos(p),msg(m),note(n),type(e){}
 };
