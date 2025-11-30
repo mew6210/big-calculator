f6427e00825d06fce9a6763e5bd2f123e52f2dfa
mew6210 <mew.62@wp.pl>
2025-11-30 16:40:32 +0100

    feat(eval): error handling for evaluator

--- src/eval/eval.cpp
+++ src/eval/eval.cpp
@@ -1,10 +1,57 @@
 #include "eval.hpp"
 #include "../astnodes/astnodes.hpp"
 #include "../logging/logging.hpp"
+#include <optional>
+
+class EvalException : public std::runtime_error {
+
+	std::string msg;
+	std::string note;
+	std::optional<BigInt> a,b;
+	std::optional<char> op;
+
+public:
+	EvalException(const std::string& _msg,const std::string& _note): 
+		std::runtime_error(_msg),
+		msg(_msg),
+		note(_note){}
+	EvalException(const std::string& _msg,const std::string& _note,BigInt& _a,BigInt& _b, const char _op) 
+		: std::runtime_error(_msg),
+		msg(_msg),
+		note(_note),
+		a(_a),
+		b(_b),
+		op(_op){}
+
+	
+	void printEvalErr(){
+		if (a && b && op) {
+			std::string src = a.value().toString() + op.value() + b.value().toString();
+
+			printError(ErrMsg{
+				src,
+				a.value().toString().size(),
+				msg,
+				note,
+				ErrType::Evaluator
+				});
+		} 
+		else {
+			std::cout << "Evaluator error: " << msg << "\n";
+			std::cout << "Note: " << note << "\n";
+		}
+	}
+};
 
 void Evaluator::eval() {
-	BigInt res = ASTRoot->eval();
-	res.print();
+	try {
+		BigInt res = ASTRoot->eval();
+		res.print();
+	}
+	catch (EvalException& e) {
+		e.printEvalErr();
+	}
+	
 }
 
 BigInt BigIntNode::eval() {
@@ -19,8 +66,10 @@ BigInt BinaryExprNode::eval(){
 	case OperatorType::add: a.addBigInt(b); break;
 	case OperatorType::subtract: a.subtractBigInt(b); break;
 	case OperatorType::multiply: a.multiplyBigInt(b); break;
-	case OperatorType::divide: break;	//TODO: USE DIVISION ONCE BIGINT DIVISION IS DONE
-	default: std::cout << "op not supported yet";
+	case OperatorType::divide: throw EvalException("division not yet supported","dont use it please",a,b,'/'); break;	//TODO: USE DIVISION ONCE BIGINT DIVISION IS DONE
+	case OperatorType::exponentiate: throw EvalException("exponentiation not yet supported", "dont use it please", a, b, '^'); break;	//TODO: EXPONENTIATION
+	case OperatorType::assign: throw EvalException("assignment not yet supported", "dont use it please", a, b, '='); break;	//TODO: ASSIGNMENT
+	case OperatorType::undefined: throw EvalException("Unkown operation", "avalible operations are: +,-,*,/,^", a, b, '?'); break;
 	}
 
 	return a;
